11d84100b5ac9550452e869d5355cc71c7df2d1d
bobwei <bob61016@gmail.com>
2016-05-30 15:24:43 +0800

    update .babelrc, add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './components/Main';
+import './styles/App.scss';
+import configureStore from './stores/configureStore';
 
-require('./styles/App.scss');
+/* initialize */
+// to allow :active styles to work in your CSS on a page in Mobile Safari
+document.addEventListener('touchstart', () => {}, true);
+const store = configureStore({});
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ src/stores/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import rootReducer from '../reducers';
+
+
+export default function configureStore(initialState) {
+  let middlewares = [ thunkMiddleware ];
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
+}
