85592d109898e8ba34a87175de6cdeec733f9abf
bobwei <bob61016@gmail.com>
2016-05-30 14:19:20 +0800

    update readme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
-// require('normalize.css/normalize.css');
 require('./styles/App.scss');
 
 ReactDOM.render(<App />, document.getElementById('app'));
