9736bfddf153c9449d365a32de36988e071231b8
bobwei <bob61016@gmail.com>
2016-05-30 22:53:28 +0800

    add server side rendering

--- server/middlewares/render.js
+++ server/middlewares/render.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import path from 'path';
+import React from 'react';
+import createMemoryHistory from 'history/lib/createMemoryHistory';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+
+import createRoutes from '../../src/routes/index';
+import configureStore from '../../src/stores/configureStore';
 
 
 export default function() {
@@ -7,8 +15,30 @@ export default function() {
   app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
   app.get('*', (req, res) => {
-    res.render('index', {
-      html: ''
+    const history = createMemoryHistory();
+    const routes = createRoutes(history);
+    const store = configureStore({});
+
+    match({
+      routes,
+      location: req.url
+    }, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else if (redirectLocation) {
+        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      } else if (renderProps) {
+        const html = renderToStaticMarkup(
+          <Provider store={store} >
+            <RouterContext {...renderProps} />
+          </Provider>
+        );
+        res.render('index', {
+          html
+        });
+      } else {
+        res.status(404).send('Not found');
+      }
     });
   });
   return app;
